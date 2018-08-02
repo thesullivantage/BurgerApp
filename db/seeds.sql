344c59c6820ad782642731c8ceb0dcfe2d447d89
Your Name <jack.sullivan96@gmail.com>
2018-08-01 22:03:12 -0400

    reworked

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devoured) VALUES ("Quesidilla Burger", false)
+INSERT INTO burgers (name, devoured) VALUES ("Quadruple Bypass Burger", false)
+INSERT INTO burgers (name, devoured) VALUES ("Big Mac", false)
+INSERT INTO burgers (name, devoured) VALUES ("Kobe Burger", false)
